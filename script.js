66e35ae1089e7b3bdf6552afd16218b8ba356507
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-02-21 16:40:34 -0700

    attempt to add button to change text color

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-/*	document.getElementById("lorem").onclick = changeColor;
+	document.getElementById("lorem").onclick = changeColor;
 	var currentColor = "blue";
 	function changeColor() {
-		if(currentColor == "blue"){
-			document.lorem.style.color = "green";
+		if(currentColor === "blue"){
+			document.body.style.color = "green";
 			currentColor = "green";
 		} else {
-			document.lorem.style.color  = "blue";
+			document.body.style.color  = "blue";
 			currentColor = "blue";
 		}
-	}*/
+	}
 
 /*function blueText() {
 	var coloredText = document.getElementsByClassName("lorem");
 	coloredText.style.color = "blue";
 }*/
 
+/*document.getElementsById("lorem").onclick = blueText;
 
+function blueText(){
+	document.lorem.style.color = "blue";
+}*/
